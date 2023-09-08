922be589c3959f26b4fc1ecd1e1418c725b3066c
Cooper Oh <cooper@chequer.io>
2023-09-08 15:01:10 +0900

    Drop qd-migrate feature

--- src/refined-jira.tsx
+++ src/refined-jira.tsx
@@ -1,7 +1,6 @@
 import './primer.css';
 import './refined-jira.css';
 import './helpers/feature-manager.js';
-import './features/qd-migrate/qd-migrate.js';
 import './features/dim-backlog/dim-backlog.js';
 import './features/sprint-date/sprint-date.js';
 import './features/issue-related-gh/issue-related-gh.js';
